daef6719b5f0e0edc32f37dfb69fd58414fdf8a3
EstebanU <eu@u.northwestern.edu>
2016-11-29 22:58:48 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -27,14 +27,3 @@ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
 title(ylab = "Energy sub metering")
 dev.copy(png, "plot3.png", width = 480, height = 480, units = "px")
 dev.off()
-
-
-# with(powerConsumption, {plot(Time, Sub_metering_1, type = "l", ylim = c(0, 38), 
-#                              xlab = "", ylab = "")})
-# 
-# par(new = TRUE)
-# with(powerConsumption, plot(Time, Sub_metering_2, type = "l", col = "red",
-#                              xlab = "", ylab = "", ylim = c(0, 38)))
-# par(new = TRUE)
-# with(powerConsumption, plot(Time, Sub_metering_3, type = "l", col = "blue",
-#                              xlab = "", ylab = "", ylim = c(0, 38)))
